daa402a8a39ea75e687a70e70cf339db1e41981c
Aigerim <aygerim083@gmail.com>
2022-11-22 16:14:52 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+    Student [] students= {new Student("Aigul","Bektenova","Female",LocalDate.of(2022,11,15)),
+            new Student("Aigul","Bektenova","Female",LocalDate.of(2022,11,15)),
+                    new Student("Aigul","Bektenova","Female",LocalDate.of(2022,11,15))};
+        for (Student s:students) {
+            System.out.println(s);
+        }
+
+
+    }
+ //   LocalDate dateofStart=LocalDate.of(2022)
+    public void Getİnfo(){
+  //      System.out.println(LocalDate.now()-LocalDate.of());
+    }
+
+}
